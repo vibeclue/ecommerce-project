177d883920ba84842ea24bbea2ec0f4451d62afd
vibeclue <most.possible@mail.ru>
2026-02-26 14:24:08 +0300

    fetch data from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
