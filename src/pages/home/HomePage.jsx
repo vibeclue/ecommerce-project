0573c8c51df4ed5f27cd9a16bb14b7fe27cc3f11
vibeclue <most.possible@mail.ru>
2026-02-27 11:18:37 +0300

    Convert home and checkout to async await

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,9 +8,12 @@ function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+
+    getHomeData();
   }, []);
 
   return (
