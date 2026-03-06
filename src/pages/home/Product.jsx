0f32c862b0e3d5ff4326eb963f0377be51eb83dc
vibeclue <most.possible@mail.ru>
2026-03-06 15:27:24 +0300

    Create integration test for HomePage

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -27,7 +27,7 @@ function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container" data-testid="product-container">
       <div className="product-image-container">
         <img
           className="product-image"
